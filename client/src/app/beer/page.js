f27ad805628601fba97b58a7339620a7382e6e08
Nagi132 <nagi132@gmail.com>
2025-07-07 18:36:26 -0400

    fix svg arrow

--- client/src/app/beer/page.js
+++ client/src/app/beer/page.js
@@ -38,12 +38,12 @@ export default function RiceLagerPage() {
                 {/* Image container with arrows */}
                 <div className="relative aspect-[3/4] w-full">
                   <button
-                    onClick={() => rotateImage('prev')}
+                    onClick={() => rotateImage('next')}
                     className="absolute left-4 top-1/2 transform -translate-y-1/2 z-10 p-2 bg-white/70 rounded-full hover:bg-white transition-colors"
-                    aria-label="Previous image"
+                    aria-label="Next image"
                   >
-                    <svg fill="#000000" height="24px" width="24px" version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" xmlnsXlink="http://www.w3.org/1999/xlink" viewBox="0 0 330.002 330.002" xmlSpace="preserve" transform="matrix(-1, 0, 0, 1, 0, 0)">
-                      <path id="XMLID_226_" d="M233.252,155.997L120.752,6.001c-4.972-6.628-14.372-7.97-21-3c-6.628,4.971-7.971,14.373-3,21 l105.75,140.997L96.752,306.001c-4.971,6.627-3.627,16.03,3,21c2.698,2.024,5.856,3.001,8.988,3.001 c4.561,0,9.065-2.072,12.012-6.001l112.5-150.004C237.252,168.664,237.252,161.33,233.252,155.997z"></path>
+                    <svg fill="#000000" height="24px" width="24px" version="1.1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 330.002 330.002">
+                      <path d="M96.75,155.997L209.25,6.001c4.972-6.628,14.372-7.97,21-3c6.628,4.971,7.971,14.373,3,21 l-105.75,140.997L233.25,306.001c4.971,6.627,3.627,16.03-3,21c-2.698,2.024-5.856,3.001-8.988,3.001 c-4.561,0-9.065-2.072-12.012-6.001l-112.5-150.004C92.75,168.664,92.75,161.33,96.75,155.997z"/>
                     </svg>
                   </button>
 
@@ -69,12 +69,12 @@ export default function RiceLagerPage() {
 
                   {/* Right Arrow */}
                   <button
-                    onClick={() => rotateImage('next')}
+                    onClick={() => rotateImage('prev')}
                     className="absolute right-4 top-1/2 transform -translate-y-1/2 z-10 p-2 bg-white/70 rounded-full hover:bg-white transition-colors"
-                    aria-label="Next image"
+                    aria-label="Previous image"
                   >
-                    <svg fill="#000000" height="24px" width="24px" version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" xmlnsXlink="http://www.w3.org/1999/xlink" viewBox="0 0 330.002 330.002" xmlSpace="preserve">
-                      <path id="XMLID_226_" d="M233.252,155.997L120.752,6.001c-4.972-6.628-14.372-7.97-21-3c-6.628,4.971-7.971,14.373-3,21 l105.75,140.997L96.752,306.001c-4.971,6.627-3.627,16.03,3,21c2.698,2.024,5.856,3.001,8.988,3.001 c4.561,0,9.065-2.072,12.012-6.001l112.5-150.004C237.252,168.664,237.252,161.33,233.252,155.997z"></path>
+                    <svg fill="#000000" height="24px" width="24px" version="1.1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 330.002 330.002">
+                      <path d="M233.252,155.997L120.752,6.001c-4.972-6.628-14.372-7.97-21-3c-6.628,4.971-7.971,14.373-3,21 l105.75,140.997L96.752,306.001c-4.971,6.627-3.627,16.03,3,21c2.698,2.024,5.856,3.001,8.988,3.001 c4.561,0,9.065-2.072,12.012-6.001l112.5-150.004C237.252,168.664,237.252,161.33,233.252,155.997z"/>
                     </svg>
                   </button>
                 </div>
